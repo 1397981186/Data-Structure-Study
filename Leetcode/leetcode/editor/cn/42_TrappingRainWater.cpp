df0bfb76d37a234e8eadec28d3ba9295b361196f
qq1397981186 <1397981186@qq.com>
2022-06-02 22:41:04 +0800

    42，84
    42，84

--- Leetcode/leetcode/editor/cn/42_TrappingRainWater.cpp
+++ Leetcode/leetcode/editor/cn/42_TrappingRainWater.cpp
@@ -3,27 +3,20 @@
 
  
 
- 示例 1： 
-
- 
-
- 
+ 示例 1：
 输入：height = [0,1,0,2,1,0,1,3,2,1,2,1]
 输出：6
 解释：上面是由数组 [0,1,0,2,1,0,1,3,2,1,2,1] 表示的高度图，在这种情况下，可以接 6 个单位的雨水（蓝色部分表示雨水）。 
  
 
- 示例 2： 
-
- 
+ 示例 2：
 输入：height = [4,2,0,3,2,5]
 输出：9
  
 
  
 
- 提示： 
-
+ 提示：
  
  n == height.length 
  1 <= n <= 2 * 10⁴ 
@@ -46,7 +39,25 @@ using namespace std;
 class Solution {
 public:
     int trap(vector<int>& height) {
+        vector<int> pool;
+        int pre=0;
+        int firstIt=0;
+        int lastIt=0;
+        int res=0;
+        int i=0;
+        while (true){
+            if (height[i]>=pre&&firstIt==0){//
+                pre=height[i];
+            }else if (height[i]<pre&&firstIt==0){
 
+            }
+
+
+
+            if (i==height.size()-1){
+                break;
+            }
+        }
     }
 };
 //leetcode submit region end(Prohibit modification and deletion)
