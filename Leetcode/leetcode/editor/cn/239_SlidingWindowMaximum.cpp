dd26b10c70378ba377d1170024d65279d185b1ed
qq1397981186 <1397981186@qq.com>
2022-07-17 22:29:49 +0800

    239
    239

--- Leetcode/leetcode/editor/cn/239_SlidingWindowMaximum.cpp
+++ Leetcode/leetcode/editor/cn/239_SlidingWindowMaximum.cpp
@@ -32,9 +32,13 @@
 */
 //思考
 //如何遍历：数组下标、队列
-//如何求最大：简单的三个三个求复杂度n*k、
-//单调队列，为什么不用单调栈？
-//从大到小维护两个元素就够了？
+//如何求最大：简单的三个三个求复杂度n*k、使用单调双向队列复杂度小于n*K
+//单调队列，为什么不用单调栈？ 此处是双向的deque，需要头、尾都能弹出元素
+//从大到小维护两个元素就够了？ 不是两个，窗口是几就最多维护几个
+
+//注意：
+//队列的用法
+//类中的私有类
 
 
 #include <iostream>
@@ -48,10 +52,50 @@ using namespace std;
 
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
+    /**
+    		执行耗时:204 ms,击败了88.30% 的C++用户
+			内存消耗:131.5 MB,击败了46.32% 的C++用户
+    */
+private:
+    class MQueue{
+        deque<int> myQueue;
+        public://C++中，类的默认访问权限是private，结构体的默认访问权限是public。
+            void pop(int num){
+                if (myQueue.front()==num){
+                    myQueue.pop_front();
+                }
+            }
+
+            int front(){
+                return myQueue.front();
+            }
+
+            void push(int num){
+                while (myQueue.size()!=0&&num>myQueue.back()){//注意此处是从back（队列尾部，尾部最后出）进行判断；这样写代码更加简洁
+                    myQueue.pop_back();
+                }
+                myQueue.push_back(num);
+            }
+    };
+
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        MQueue myQueue;
+        vector<int> res;
+        for (int i = 0; i <= k - 1; i++) {
+            myQueue.push(nums[i]);
+        }
+        res.push_back(myQueue.front());
+        for (int j = k; j <= nums.size() - 1; j++) {
+            myQueue.pop(nums[j-k]);
+            myQueue.push(nums[j]);
+            res.push_back(myQueue.front());
+        }
 
+        return res;
     }
+
+
 };
 //leetcode submit region end(Prohibit modification and deletion)
 int main()
